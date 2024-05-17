48e593d533bc53226d8e0c8fa7eef30fe5774a79
CASTIMAX <castimax@gmail.com>
2024-05-17 21:00:20 +0200

    Update streamlit_app.py
    Añadido funcion de espera, con tiempos para tiempos de procesamiento largos

--- groq/API_main2/streamlit_app.py
+++ groq/API_main2/streamlit_app.py
@@ -6,12 +6,12 @@ from aiocache.serializers import JsonSerializer
 # Configuración inicial de aiocache para usar Redis
 # (Descomentar las siguientes líneas cuando esté listo para usar Redis)
 # import aioredis
-# 
+#
 # async def get_cache():
 #     redis = await aioredis.create_redis_pool("redis://localhost:6379")
 #     cache = Cache(Cache.REDIS, endpoint="localhost", port=6379, namespace="main", redis=redis, serializer=JsonSerializer())
 #     return cache
-
+#
 # cache = get_cache()
 
 st.title("Demostración de Chatgroq con Llama3")
@@ -25,18 +25,26 @@ if st.button("Obtener respuesta"):
     # if cached_response:
     #     response_data = cached_response
     # else:
-    #     response = requests.post("http://localhost:8000/ask", json={"question": prompt1})
-    #     response_data = response.json()
-    #     await cache.set(prompt1, response_data, ttl=60*60)  # Cachea por 1 hora
+    #     with st.spinner("Procesando la pregunta..."):
+    #         response = requests.post("http://localhost:8000/ask", json={"question": prompt1})
+    #         response_data = response.json()
+    #     await cache.set(prompt1, response_data, ttl=60*60) # Cachea por 1 hora
 
     # Sin Redis, sólo llamando directamente al API
-    response = requests.post("http://localhost:8000/ask", json={"question": prompt1})
-    response_data = response.json()
+    with st.spinner("Procesando la pregunta..."):
+        response = requests.post("http://localhost:8000/ask", json={"question": prompt1})
+        response_data = response.json()
 
     st.write("Respuesta:")
-    st.write(response_data['respuesta'])
+    with st.spinner("Generando la respuesta..."):
+        st.write(response_data['respuesta'])
+
+    st.write(f"Tiempo de procesamiento: {response_data['tiempo_procesamiento']:.2f} segundos")
 
     with st.expander("Detalles adicionales"):
         st.write("Contexto:")
         for step in response_data["contexto"]:
             st.write(step)
+
+    st.success("¡Respuesta generada con éxito!")
+    st.balloons()
